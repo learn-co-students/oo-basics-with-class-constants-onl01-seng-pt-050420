e4c94bb0170fc7cf9116afb902047f43fa4266aa
Zachary Lynch <zachary.lynch1337@gmail.com>
2020-05-18 18:31:37 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,6 +1,9 @@
+require 'pry'
 class Shoe
   attr_accessor :color, :size, :material, :condition
-  attr_reader :brand
+  # attr_reader :brand
+  
+  BRANDS = []
 
   def initialize(brand)
     @brand = brand
@@ -10,5 +13,12 @@ class Shoe
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
+  
+  def brands=(brand)
+    @brand = brand
+    BRANDS << @brand
+    binding.pry
+  end
 
 end
+
