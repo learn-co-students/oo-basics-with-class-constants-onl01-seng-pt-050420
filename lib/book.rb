584b024b9c3f5131a0ff6bd0d921fbd1937bd2a9
travis Lawson <travislawclimb@gmail.com>
2020-06-07 18:37:01 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,22 +1,25 @@
-
-
 class Book
-  attr_accessor :author, :page_count  
-  attr_reader :title, :genre  
- 
+  
+  attr_accessor :author, :page_count
+  attr_reader :title, :genre
+  
   GENRES = []
- 
+  
   def initialize(title)
-    @title = title
-  end
- 
+    @title = title 
+  end 
+  
+  def title
+    @title
+  end 
+  
   def turn_page
-    puts "Flipping the page...wow, you read fast!"
+     puts "Flipping the page...wow, you read fast!"
   end
- 
- 
+  
   def genre=(genre)
-    @genre = genre
+    @genre = genre 
     GENRES << genre 
-  end
-end
+  end   
+ 
+end   
